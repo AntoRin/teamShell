4e88304db44ee5a1f46d678dd9fc5d0773b9888b
AntoRin <antorinish@gmail.com>
2021-03-29 21:46:12 +0530

    deleted isLoggedIn utility fn, changed functionality to a middleware

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,16 +1,13 @@
 const { Router } = require("express");
 const mongoose = require("mongoose");
 const User = require("../models/User");
-const isLoggedIn = require("../utils/isLoggedIn");
 
 const router = Router();
 
 router.get("/details/:UniqueUsername", async (req, res) => {
    let requestedUser = req.params.UniqueUsername;
-
+   console.log(req.thisUser);
    try {
-      let loggedInStatus = await isLoggedIn(req.cookies.token);
-      if (loggedInStatus instanceof Error) throw loggedInStatus;
       let user = await User.findOne({ UniqueUsername: requestedUser });
       if (user) return res.json({ status: "ok", user });
       else throw "User not found";
